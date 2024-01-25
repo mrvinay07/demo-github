aa54a7e30087cb9db43fe5ef3589e02d6a511363
sharmavinay2108@gmail.com <vinaysharma@192.168.1.120>
2024-01-25 15:17:57 +0530

    revert something

--- home.py
+++ home.py
@@ -2,9 +2,8 @@ user_name = "vivek sharma"
 
 user_email = "abc@gmail.com"
 
-def get_info():
-    return user_name + user_email
+
 
 def update_info():
-    print("Information is updated")
+    prin("Information is updated")
 
--- utils/utils.py
+++
@@ -1,3 +0,0 @@
-def calculater():
-    return "Calculater functionality enabled"
-
